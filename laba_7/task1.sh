76cce2d79587f7a2dad5f48470dc0ceed4b1296f
papashik <96551531+papashik@users.noreply.github.com>
2023-01-21 19:06:32 +0300

    Update task1.sh

--- laba_7/task1.sh
+++ laba_7/task1.sh
@@ -1,23 +1,39 @@
 #!/bin/bash
+
+for ind in {1..100}
+do
+  if [[ ! -e "errors$ind.txt" && ! -e "output$ind.txt" ]]
+  then
+    ERRORSFILENAME="errors$ind.txt"
+    OUTPUTFILENAME="output$ind.txt"
+    break
+  fi
+done
+
 if [ $# -lt 2 ]
 then
-  echo "необходимое количество аргументов - 2" >> errors.txt
+  echo "необходимое количество аргументов - 2" >> $ERRORSFILENAME
   exit 1
 elif ! [ $1 -eq $1 ]
 then
-  echo "интервал не является числом" >> errors.txt
+  echo "интервал не является числом" >> $ERRORSFILENAME
   exit 1
 elif ! [ -f $2 ]
 then 
-  echo "не найден файл с программой" >> errors.txt
+  echo "не найден файл с программой" >> $ERRORSFILENAME
   exit 1
 else
+  $2 >> $OUTPUTFILENAME &
+  VAR=$!
   while true
   do 
-    $2 >> script_output.txt &
-    wait -n
-    echo "end, new process"
     sleep $1s
-    wait -n
+    if [[ -z $(ps -p $VAR | grep "Done") ]]
+    then
+      continue
+    fi    
+    echo "end, new process"
+    $2 >> $OUTPUTFILENAME &
+    VAR=$!
   done 
 fi
